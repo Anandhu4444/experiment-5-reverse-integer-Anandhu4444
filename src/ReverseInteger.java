7d7553c0068e320c9a0778fc27279ba6af5145d5
Anandhu4444 <hey4444567889987@gmail.com>
2026-03-12 16:42:15 +0530

    Update ReverseInteger.java

--- src/ReverseInteger.java
+++ src/ReverseInteger.java
@@ -1,18 +1,20 @@
-import java.util.*;
-public class ReverseInteger { 
-        public static int reve(int num){
-        int rev=0;
-        while(num>0){
-                int digit=num%10;
-                rev=rev*10+digit;
-                num=num/10;
+import java.util.Scanner;
+
+public class Reverse {
+    static void reverse(int n){
+        int temp1,temp2=0;
+        while(n!=0){
+            temp1=n%10;
+            n=n/10;
+            temp2=temp2*10+temp1;
         }
-        return rev;
-        }
-        public static void main(String args[]){
+        System.out.println(temp2);
+    }
+
+    public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the number ");
-        int num=sc.nextInt();
-        System.out.println(reve(num));
-        }
+        int n=sc.nextInt();
+        reverse(n);
+    }
 }
+
